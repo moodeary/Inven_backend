226071020121f85501b310d3302298044f0860ad
choonsik <moodeary@gmail.com>
2025-09-17 23:39:33 +0900

    update

--- src/main/java/com/basic/template/backend_template/common/config/WebConfig.java
+++ src/main/java/com/basic/template/backend_template/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:5173")
+                .allowedOrigins("http://localhost:3001", "http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
